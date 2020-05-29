c89cc31092fe51d787f47d3ff854e21a0f1845e3
Stefan Zhelyazkov <stefan.zhelyazkov@gmail.com>
2020-05-29 14:10:14 +0300

    Updates

---
+++ precache-manifest.ac278ff616e10916421aa6732f9210fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83997b609bc587db2dd8f3730aefc6d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "373e70bac7531797c43e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "838d2870889ac3c0d94b",
+    "url": "/static/js/2.9b059db9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b059db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373e70bac7531797c43e",
+    "url": "/static/js/main.df24ec11.chunk.js"
+  },
+  {
+    "revision": "7dc80f33791093d5ec83",
+    "url": "/static/js/runtime-main.1a0fa255.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
